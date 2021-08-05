73de1d0111e561f48144eda0baf04121d5e32c5f
ore291 <oreoluwapadonu@gmail.com>
2021-08-05 15:01:07 +0100

    v1

---
+++ src/components/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+//  config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+//  components
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
+import Spinner from "./Spinner";
+//  hooks
+import { useHomeFetch } from "../hooks/useHomeFetch";
+// image
+import NoImage from "../images/no_image.jpg";
+
+const Home = () => {
+  const { state, loading, error } = useHomeFetch();
+
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <Grid header="Popular Movies">
+        {state.results.map((movie) => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                : NoImage
+            }
+            movieId={movie.id}
+          ></Thumb>
+        ))}
+      </Grid>
+      <Spinner />
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,3 @@
+import React, {useEffect, useState, useRef} from 'react';
+import {Wrapper, logo, Content } from './SearchBar.styles';
+
